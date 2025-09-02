7a9e5c737a953538c0a4d622549ef9124db19dff
neecklace <niicolas.bertacin@gmail.com>
2025-09-02 09:47:14 -0300

    update defs

--- defs.py
+++ defs.py
@@ -34,7 +34,7 @@ def adicionar_livro():
     titulo=input("Nome do livro -->").capitalize()
     autor=input("Autor -->").capitalize()
     genero=input("Genero -->").capitalize()
-    novo_id = max(dicionario.keys()) + 1 #pega o ultimo id e adiciona 1 para o novo livro
+    novo_id = max(dicionario.keys()) + 1 #pega o ultimo id e adiciona 1 para o novo livro.
     novo_livro=Livro(titulo, autor, genero) #adiciona uma variavel chamada novo_livro
     dicionario[novo_id]= novo_livro #adiciona dentro do novo id, as informaçoes perticentes a variavel novo_livro.
     print(f"Novo livro adicionado com sucesso! Está localizado na Id {novo_id}")
